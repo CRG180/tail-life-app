5c7a0a27b99244ec3ee33053def9a293f17f03c8
charles.gallagher07 <charles.gallagher07@gmail.com>
2022-11-30 06:36:10 -0600

    update models add start templates

--- TLsite/leaderSignUp/models.py
+++ TLsite/leaderSignUp/models.py
@@ -8,19 +8,35 @@ class trailGuideLeader(models.Model):
     email_address = models.EmailField(max_length=254)
     phone_number = PhoneField(blank=True, help_text='Contact phone number')
 
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}, {self.middle_initial}."
+
 class eventType(models.Model):
     event_category = models.CharField(max_length=200) # weekday meeting, special event
+
+
+
+    def __str__(self):
+        return f"{self.event_category}"
+
 
 class group(models.Model):
     group_name = models.CharField(max_length=200) # hawks A, Hawks B, Moutain Lions
 
+    def __str__(self):
+        return f"{self.group_name}"
+
 class event(models.Model):
     title = models.CharField(max_length=200)
+    eventType = models.ForeignKey(eventType, on_delete=models.CASCADE, default= '' )
     description = models.CharField(max_length=1000)
     start = models.DateTimeField('Event start time')
     end = models.DateTimeField('Event end time')
     primary_leader = models.ForeignKey(trailGuideLeader, on_delete=models.RESTRICT, related_name='leader')
     alternate_leader = models.ForeignKey(trailGuideLeader, on_delete=models.RESTRICT, related_name='alternate_leader')
+    
+    def __str__(self):
+        return f"{self.title}, {self.start} ---- {self.eventType}"
 
 class subEvent(models.Model):
     title = models.CharField(max_length=200)
