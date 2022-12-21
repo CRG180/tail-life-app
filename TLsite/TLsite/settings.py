b44c3958cbbef510827c562d27594ead27b1f88c
charles.gallagher07 <charles.gallagher07@gmail.com>
2022-12-21 14:06:28 -0600

    dockerize the app for both Django and postgres

--- TLsite/TLsite/settings.py
+++ TLsite/TLsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xa&f$1old7+&i2&7ecw$=sx!$ace+@j92zlcg+2%!f(jt7g7k+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',0)))
 
-ALLOWED_HOSTS = ['0.0.0.0', '192.168.1.4']
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
@@ -77,10 +83,11 @@ WSGI_APPLICATION = 'TLsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '../.mysql.cnf',
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
--- TLsite/leaderSignUp/admin.py
+++ TLsite/leaderSignUp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import trailGuideLeader, eventType, group, event
+from .models import trailGuideLeader, trailman ,eventType, group, event
 from .models import subEvent
 admin.site.register(trailGuideLeader)
+admin.site.register(trailman)
 admin.site.register(eventType)
 admin.site.register(group)
 admin.site.register(event)
