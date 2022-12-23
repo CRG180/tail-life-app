eb0b005f810e3e9a4941dc15a4c26bb3482fe253
charles.gallagher07 <charles.gallagher07@gmail.com>
2022-12-22 22:24:32 -0600

    create the  deploy proxy docker compose

--- TLsite/leaderSignUp/urls.py
+++ TLsite/leaderSignUp/urls.py
@@ -3,9 +3,16 @@ from django.urls import path
 from leaderSignUp.views import indexView
 from leaderSignUp.views import eventView
 from leaderSignUp.views import subEventView
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', indexView, name = 'index'),
     path('events/<int:event_id>/',eventView, name = 'events'),
     path('events_detail/<int:subEvent_id>/',subEventView, name = 'subEvents')
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT,
+        )
